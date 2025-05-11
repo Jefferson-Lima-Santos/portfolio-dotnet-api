ea97f3dd7d898f19b3ee93d6a7a6a9243a4f956c
jefferson.santos <jeffersonlima2020s@gmail.com>
2025-05-11 14:25:32 -0300

    ApiController para Padronização de Resposta da aplicação

---
+++ src/Portfolio.Services.Api/Controllers/ApiController.cs
@@ -0,0 +1,48 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Api.Domain.Mediator.Notifications;
+using Portfolio.Services.Api.Response;
+
+namespace Portfolio.Services.Api.Controllers
+{
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        protected ILogger _logger;
+        protected readonly DomainNotificationHandler _notifications;
+
+        protected ApiController(ILogger logger, INotificationHandler<DomainNotification> notifications)
+        {
+            _logger = logger;
+            _notifications = (DomainNotificationHandler)notifications;
+        }
+
+        protected ActionResult ResponseApi(object? result = null)
+        {
+            if (!ModelState.IsValid)
+            {
+                for (var i = 0; i < ModelState.Count; i++)
+                    _notifications.Handle(
+                        new DomainNotification(ModelState.Keys.ToList()[i],
+                            ModelState.Values.ToList()[i].Errors.FirstOrDefault().ErrorMessage),
+                        CancellationToken.None);
+            }
+
+            if (!_notifications.HasNotifications())
+            {
+                return Ok(new Result
+                {
+                    Succeeded = true,
+                    Data = result
+                });
+            }
+
+            return BadRequest(new Result
+            {
+                Succeeded = false,
+                Errors = _notifications.GetNotifications()
+                    .Select(n => new MessageError { Key = n.Key, Message = n.Value }).ToArray()
+            });
+        }
+    }
+}
